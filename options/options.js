a1838331bace2f9d4a9c3b9947df327bb5764240
Crowdbotics <team@crowdbotics.com>
2021-08-29 00:34:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "frosty_tree_30169Identifier",
+  url: "https://frosty_tree_30169Identifier.botics.co",
+  api: "https://frosty_tree_30169Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
